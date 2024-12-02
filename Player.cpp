4f9f6bedd9df0287bc08dce9de0da09b26b2f909
Mustafa Haider <haides35@mcmaster.ca>
2024-12-01 22:02:56 -0500

    Movement Added

--- Player.cpp
+++ Player.cpp
@@ -27,13 +27,65 @@ objPos Player::getPlayerPos() const
 
 void Player::updatePlayerDir()
 {
-
+    // Copied from PPA 3 Logic
+    char input = mainGameMechsRef->getInput();
+    switch(input) { 
+        case 'w':
+            if(myDir != DOWN) 
+                myDir = UP;
+                break;
+        case 'a':
+            if(myDir != RIGHT) 
+                myDir = LEFT;
+                break;
+        case 's':
+            if(myDir != UP) 
+                myDir = DOWN;
+                break;
+        case 'd':
+            if(myDir != LEFT) 
+                myDir = RIGHT;
+                break;
+        default:
+            break;
+    }  
 
 }
 
 void Player::movePlayer()
 {
-    
+    // PPA3 Finite State Machine logic
+    int px = playerPos.pos->x;
+    int py = playerPos.pos->y;
+    int boardLimitY = mainGameMechsRef->getBoardSizeY()-2;
+    int boardLimitX = mainGameMechsRef->getBoardSizeX()-2;
+
+    // Logic for movement copied directly from PPA3
+    switch(myDir)
+    {                      
+        case DOWN: 
+            py = (py % boardLimitY)+1;
+            break;
+            
+        case UP:
+            py--;
+            if(py < 1) {py = boardLimitY;}
+            break;
+            
+        case LEFT:
+            px--;
+            if(px < 1) {px = boardLimitX;}
+            break;
+
+        case RIGHT:
+            px = (px % boardLimitX)+1;
+            break;
+
+        default:
+            break;
+    }
+
+    playerPos.setObjPos(px, py, playerPos.symbol);
 }
 
 // More methods to be added
